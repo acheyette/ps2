37a6175743bad212f77c73845bdb6be490e8dedf
acheyette <48032616+acheyette@users.noreply.github.com>
2019-02-26 15:53:56 -0500

    Delete ps2.R

--- ps2.R
+++
@@ -1,8 +0,0 @@
-setwd('/Users/annacheyette 1/econ 104/ps2')
-
-CATestScoreData<-read.csv("~/econ 104/ps2/CATestScoreData.csv")
-
-#defining variables
-reading<-CATestScoreData$read_scr
-size<-CATestScoreData$str
-
