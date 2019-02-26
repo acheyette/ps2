5bbaf493c4134265b8e150599bba6f9bb1af7a87
acheyette <48032616+acheyette@users.noreply.github.com>
2019-02-26 15:48:17 -0500

    Add files via upload

---
+++ ps2.R
@@ -0,0 +1,8 @@
+setwd('/Users/annacheyette 1/econ 104/ps2')
+
+CATestScoreData<-read.csv("~/econ 104/ps2/CATestScoreData.csv")
+
+#defining variables
+reading<-CATestScoreData$read_scr
+size<-CATestScoreData$str
+
